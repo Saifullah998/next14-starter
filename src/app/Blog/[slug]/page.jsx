3b19d44dc0cbb80c95ca403e237588929fab5929
Saifullah998 <103965451+Saifullah998@users.noreply.github.com>
2024-01-25 23:37:08 +0500

    complete

--- src/app/Blog/[slug]/page.jsx
+++ src/app/Blog/[slug]/page.jsx
@@ -1,5 +1,35 @@
+import Image from "next/image";
+import styles from "./singlePost.module.css"
 const SinglePostPage  = () => {
-    return <div>SinglePostPage</div>;
+    return <div className={styles.container}>
+    <div className={styles.imgContainer}>
+      <Image src="https://images.pexels.com/photos/19841819/pexels-photo-19841819/free-photo-of-a-narrow-street-with-many-colorful-rugs-and-carpets.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" fill className={styles.img} />
+    </div>
+    <div className={styles.textContainer}>
+      <h1 className={styles.title}>Title</h1>
+      <div className={styles.avatar}>
+       <Image src="https://images.pexels.com/photos/19841819/pexels-photo-19841819/free-photo-of-a-narrow-street-with-many-colorful-rugs-and-carpets.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        alt=""
+         width={50} height={50} 
+         className={styles.avatar} />
+         <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Author</span>
+            <span className={styles.detailValue}>Terry Jefferson</span>
+         </div>
+         <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>01.01.2024</span>
+         </div>
+      </div>
+      <div className={styles.content}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+      et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+      aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse 
+      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+       sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </div>
+    </div>
+    </div>;
   };
   
   export default SinglePostPage;
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -2,6 +2,12 @@ import Image from "next/image";
 import styles from "./contact.module.css";
 
 const ContactPage = () => {
+
+
+
+
+
+  
   return (
     <div className={styles.container}>
       <div className={styles.imgContainer}>
